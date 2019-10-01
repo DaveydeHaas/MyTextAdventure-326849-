07e5432dd50bb9f1ba657168b5e1200f6096d911
Davey <326849@student.mboutrecht.nl>
2019-10-01 10:58:06 +0200

    no message

---
+++ Periode1/MyTextAdventure/TextAdventure[326849]/Assets/Scripts/MyAdventure.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyAdventure : MonoBehaviour
+{
+    private enum States
+    {
+        start,
+        Intro,
+        R1Question1,
+        R1_F,
+        R1_R,
+        R1_L,
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowMainMenu();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnUserInput(string input)
+    {
+        if (input == "start")
+        {
+            startintro();
+        }
+        else
+        {
+            Terminal.ClearScreen();
+            Terminal.WriteLine("*Type START to begin...*");
+        }
+    }
+    void ShowMainMenu()
+    {
+        Terminal.WriteLine("Welcome by Burning House");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("this game is a Text Adventure");
+        Terminal.WriteLine("That means you have to make choices");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("*Type START to begin...*");
+    }
+
+    void startintro()
+    {
+        Terminal.ClearScreen();
+        Terminal.WriteLine("test");
+    }
+}
